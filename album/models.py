321e530e39ad06f158d7a4a7fb96617dda4b43b9
KuoCH <Kuo@Mac>
2013-05-29 05:14:09 +0800

    create model

--- album/models.py
+++ album/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Picture(models.Model):
+    image = models.ImageField(upload_to="pictures/")
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    pub_date = models.DateTimeField('date published')
+
+class Comment(models.Model):
+    picture = models.ForeignKey(Picture)
+    author = models.CharField(max_length=200)
+    description = models.TextField()
+    pub_date = models.DateTimeField('date published')
